f14ef41d9046159757496aa741597710369ed5bc
Stephane Bastian <stephane.bastian@monpetitguide.com>
2015-07-29 17:48:18 +0200

    added the Flash implementation

--- src/main/java/com/thesoftwarefactory/vertx/web/mvc/impl/FlashImpl.java
+++ src/main/java/com/thesoftwarefactory/vertx/web/mvc/impl/FlashImpl.java
@@ -1,15 +1,29 @@
 package com.thesoftwarefactory.vertx.web.mvc.impl;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.thesoftwarefactory.vertx.web.mvc.Flash;
 
 public class FlashImpl implements Flash {
 
+	private final static String KEY_MESSAGES = "__messages__";
+	private final static String KEY_ERRORS = "__errors__";
+	
 	private Map<String, Object> map;
 	
+	@Override
+	public List<String> errors() {
+		List<String> result = get(KEY_ERRORS);
+		if (result==null) {
+			result = new ArrayList<>();
+		}
+		return result;
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T get(String key) {
@@ -19,6 +33,15 @@ public class FlashImpl implements Flash {
 	@Override
 	public Iterable<String> keys() {
 		return map!=null ? map.keySet() : Collections.emptyList();
+	}
+
+	@Override
+	public List<String> messages() {
+		List<String> result = get(KEY_MESSAGES);
+		if (result==null) {
+			result = new ArrayList<>();
+		}
+		return result;
 	}
 
 	@Override
