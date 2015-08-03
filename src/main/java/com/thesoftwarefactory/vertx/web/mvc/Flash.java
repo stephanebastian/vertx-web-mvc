4378ec38511971348a03bd007fcd1659ca678e79
Stephane Bastian <stephane.bastian@monpetitguide.com>
2015-08-03 15:18:55 +0200

    ordered members

--- src/main/java/com/thesoftwarefactory/vertx/web/mvc/Flash.java
+++ src/main/java/com/thesoftwarefactory/vertx/web/mvc/Flash.java
@@ -1,27 +1,26 @@
 package com.thesoftwarefactory.vertx.web.mvc;
 
-import java.util.Collection;
 import java.util.List;
 
 import com.thesoftwarefactory.vertx.web.mvc.impl.FlashImpl;
 
 public interface Flash {
 
-	public <T> T get(String key);	
+	public static Flash create() {
+		return new FlashImpl();
+	}	
 	
-	public Flash put(String key, Object value);
+	public List<String> errors();
 	
-	public <T> T remove(String key);
+	public <T> T get(String key);
 	
 	public Iterable<String> keys();
 	
-	public static Flash create() {
-		return new FlashImpl();
-	}
-	
 	public List<String> messages();
+	
+	public Flash put(String key, Object value);
 
-	public List<String> errors();
+	public <T> T remove(String key);
 
 }
 
